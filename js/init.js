4f3b4fe64f7511ea8b44b5dc3b72be5f4d7a8668
simoaweb <simoaweb@gmail.com>
2021-05-14 11:13:17 -0300

    cartao

--- js/init.js
+++ js/init.js
@@ -7,4 +7,14 @@
     $('.collapsible').collapsible();
   });
   }); // end of document ready
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
 })(jQuery); // end of jQuery name space
