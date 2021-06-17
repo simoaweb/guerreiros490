0d6cc24d65576ba5445f33d9882dc5e5b6d25a46
simoaweb <simoaweb@gmail.com>
2021-06-17 11:27:50 -0300

    feed

--- js/init.js
+++ js/init.js
@@ -17,4 +17,9 @@
   $(document).ready(function(){
     $('select').formSelect();
   });
+  (function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/e64659c9-4f4a-4852-8eaf-1b8d5e5e52f6.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
 })(jQuery); // end of jQuery name space
